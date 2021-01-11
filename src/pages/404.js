ad5c3c865ad7175885c957d97ed7ca46f90bb927
Xaver Fleer <xaver.fleer@gmail.com>
2021-01-11 22:25:31 +0100

    Copy index to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,26 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import narwhal from "../images/narwhal.png";
 
 // markup
-const NotFoundPage = () => {
+const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main
+      style={{
+        alignItems: "center",
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        margin: "0",
+      }}
+    >
+      <title>Narwahl</title>
+      <img
+        alt="Narwahl is the new Unicorn"
+        src={narwhal}
+        style={{ maxHeight: "300px" }}
+      />
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default IndexPage;
